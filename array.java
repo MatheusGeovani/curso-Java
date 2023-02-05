9ad8c33abebe03110c963d66526b97b0b7285037
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-02-04 21:56:28 -0300

    java

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args){
+        int[] numeros = {10,20,30,40,50};
+
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] == 40){
+                System.out.println(i+1);
+                break;
+            }
+        }
+    }
+}
