7883b397e16bcaf1e6347cda12c5c0eec456f975
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-02-04 21:27:17 -0300

    Initial commit

---
+++ cursoJava.java
@@ -0,0 +1,14 @@
+public class cursoJava {
+    public static void main (String[] args){
+
+        int num = 0;
+        num = 24;
+        String nome = "Matheus";
+
+        System.out.print("Matheus\n"); // não realiza quebra de linha
+        System.out.println("Curso de java!"); // realiza quebra de linha no final
+        System.out.printf("%s - %d","Matheus", 24); 
+        System.out.printf("\nValor da variavel num:%d",num);
+        System.out.printf("\nMeu nome e %s e tenho %d anos",nome,num);
+    }
+}
---
+++ decisao.java
@@ -0,0 +1,21 @@
+public class decisao {
+    public static void main(String[] args) {
+        // == > < >= <= != !
+
+        int nota = 50 ;
+        int faltas = 20;
+        int limiteFaltas = 30;
+        int media = 60;
+
+        if(nota >= media && faltas > limiteFaltas || nota >= 40 &&  nota < media) {
+            System.out.println("Voce esta de recuperacao!");
+        }
+        else if(nota >= media){
+            System.out.println("Voce foi aprovado!");
+        }
+        else {
+            System.out.println("Voce foi reprovado!");
+        }
+        System.out.println("Fim do programa!");
+    }
+}
