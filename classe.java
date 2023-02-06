b80d84c1a9645d36ab55a41408e92fce9a354674
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-02-06 07:30:04 -0300

    java

---
+++ classe.java
@@ -0,0 +1,8 @@
+public class classe {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador(1);
+        Jogador jogador2 = new Jogador(2);
+        Jogador jogador3 = new Jogador(3);
+    }
+    
+}
