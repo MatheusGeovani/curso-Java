6293368aa0542ac411381fc1272c1b755dcbe6a0
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-02-11 22:27:03 -0300

    classe aula02

--- aula02/Caneta.java
+++ aula02/Caneta.java
@@ -1,22 +1,42 @@
 package aula02;
 
 public class Caneta {
+    String nome;
     String cor;
     String modelo;
     float ponta;
     int carga;
     boolean tampada;
+    boolean rabiscado;
+
+    public Caneta(String nome){
+        this.nome = nome;
+        this.cor = "azul";
+        this.modelo = "bic";
+        this.ponta = 5;
+        this.carga = 80;
+        this.tampada = false;
+        this.rabiscado = true;
+    }
+
     void status(){
-        System.out.print("Uma caneta " + this.cor);
+        System.out.println("Uma caneta");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga + "%");
     }
 
     void rabiscar(){
-
+        if(this.tampada == true){
+            System.out.println("ERRO! Nao posso rabisca");
+        }else{
+            System.out.println("Estou rabiscando");
+        }
+        System.out.println("-----------------------------------------------------------");
     }
-    void tampar(){
-
+    void tampDestampada(){
+        System.out.println("A caneta esta tampada: " + (this.tampada ? "Sim" : "Nao"));
     }
-    void destampar(){
-        System.out.printf("\nA caneta esta tampada:", this.tampada ? "Sim" : "Nao");
-    }
+    
 }
--- aula02/aula02.java
+++ aula02/aula02.java
@@ -2,13 +2,27 @@ package aula02;
 
 public class aula02 {
     public static void main(String[] args){
-        Caneta c1 = new Caneta();
-        c1.cor = "azul";
+        Caneta c1 = new Caneta("Caneta");
+        Caneta c2 = new Caneta("Caneta");
+
+
+        c1.cor = "vermelho";
         c1.modelo = "bic";
         c1.ponta = 0.5f;
-        c1.tampada = false;
         c1.carga = 80;
         c1.status();
-        c1.destampar();
+        c1.tampDestampada();
+        c1.rabiscar(); 
+
+        c2.cor = "preto";
+        c2.modelo = "faber castelo";
+        c2.ponta = 10;
+        c2.carga = 20;
+        c2.tampada = true;
+        c2.status();
+        c2.tampDestampada();
+        c2.rabiscar();
+
+
     }
 }
